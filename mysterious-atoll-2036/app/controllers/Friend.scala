cf418322740c8fe87209e332da0249f10fed26b7
Patrick Burd <patrick@appiphony.com>
2012-11-02 16:26:06 -0500

    The server is back in the repo.

---
+++ mysterious-atoll-2036/app/controllers/Friend.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models._
+import views._
+
+
+object FriendController extends Controller with Secured{
+
+	def getFriends = IsAuthenticated { username => _ =>
+		User.findByUsername(username).map { user =>
+			Ok(xml.friends(User.getFriends(username)))
+		}.getOrElse(Forbidden)
+	}
+
+	def addFriend = TODO
+}
---
+++ mysterious-atoll-2036/app/models/Friend.scala
@@ -0,0 +1,17 @@
+package models
+
+import java.util.{Date}
+
+import play.api.db._
+import play.api.Play.current
+
+import anorm._
+import anorm.SqlParser._
+
+
+case class Friend()
+
+
+object Friend {
+	
+}
